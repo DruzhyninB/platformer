3f8c18deccfa3c0baba6fd8d1d55c054ad3eea5b
Bogdancrunch <bogdan.druzhynin@crunch.is>
2019-09-03 14:02:32 +0300

    Updates

---
+++ precache-manifest.b02b83d54aec432f4690394950bb7f9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e202f76c4e8990fa6e9a8fb5ca70ff29",
+    "url": "/platformer/index.html"
+  },
+  {
+    "revision": "79759767e54c64060644",
+    "url": "/platformer/static/css/2.5a7e7f24.chunk.css"
+  },
+  {
+    "revision": "88c975ce67f5279db434",
+    "url": "/platformer/static/css/main.1acd1a87.chunk.css"
+  },
+  {
+    "revision": "79759767e54c64060644",
+    "url": "/platformer/static/js/2.b1fd4e50.chunk.js"
+  },
+  {
+    "revision": "88c975ce67f5279db434",
+    "url": "/platformer/static/js/main.f7b1a19d.chunk.js"
+  },
+  {
+    "revision": "df3a0a86e4ed7cd4a742",
+    "url": "/platformer/static/js/runtime~main.c69ca47e.js"
+  }
+]);
